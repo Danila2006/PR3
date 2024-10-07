026132545c016427c76800b1ffde589631dab35b
Dan <118832521+Danila2006@users.noreply.github.com>
2024-10-07 11:53:46 +0300

    Added index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+<<<<<<< HEAD
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index1.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+=======
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index1.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+>>>>>>> 02452c5791e0c67d81382ce879fde9a55d29ac33
+});
